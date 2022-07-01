f3ae519115e290396638e37f5e0cadbb8639d38b
Jari Wilms <jariwilms@outlook.com>
2022-07-01 13:06:50 +0200

    first commit

---
+++ contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/jariw/Downloads/assimp-5.2.4/contrib/zlib
+# Build directory: C:/Users/jariw/Downloads/New folder/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
